83f38c76d99c7ff5136c31f7aa6df8852f2b41ac
Mattheus <Mattheus@DESKTOP-SOUHEDK>
2017-08-04 22:33:07 -0400

    Adição da tela principal e o controller da mesma

--- EngSoftControleMaquinario/src/javafxmvc/Principal.java
+++ EngSoftControleMaquinario/src/javafxmvc/Principal.java
@@ -1,11 +1,33 @@
 package javafxmvc;
 
-/**
- *
- * @author Andrei
- */
-public class Principal {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Principal extends Application {
+    
     public static void main(String[] args) {
+        Application.launch(Principal.class, args);
         
     }
+    
+    @Override
+    public void start(Stage stage) {
+        AnchorPane FrmPrincipal = null;
+        try {
+            FrmPrincipal = FXMLLoader.load(getClass().getResource("visao/tela_principal.fxml"));
+        } catch (Exception ex) {
+            System.out.println("Exception on FXMLLoader.load()");
+            System.out.println(ex.getMessage());  
+        }
+
+        stage.setTitle("Sistema de Controle de Maquinário");
+        Scene scene = new Scene(FrmPrincipal);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+    
 }
